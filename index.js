4a6ea8fcde3101f7fc342ac3f412ee5d1f0e8c9f
Leu Family <romanianfobjr@gmail.com>
2021-06-11 12:52:50 -0500

    solved task 5, skipped task 4 for now

--- index.js
+++ index.js
@@ -246,8 +246,15 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array, key, string1, string2, key2){
+  let newArr = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i][key].includes(string1 && string2)){
+      newArr.push(array[i][key2]);
+    } /* Problem arises as a string containing '- 19' will necessarily contain '19' rendering this function unusable */
+    /* How to take pieces of a string for comparison purposes */
+  }
+  return newArr;
 }
 
 
@@ -261,8 +268,9 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, index){
+       array.splice(index, 1);
+       return array.length;           
 }
    
 
